f461fb948b465a823d0a363163f165652712d9ae
VMAT <villalvama@gmail.com>
2021-08-02 19:31:58 -0300

    .

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  // uri: 'http://localhost:3000/graphql',
+  uri: 'https://securitize-api.herokuapp.com/graphql',
   cache: new InMemoryCache(),
 });
 
